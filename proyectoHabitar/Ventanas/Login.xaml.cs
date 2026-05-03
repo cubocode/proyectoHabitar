c2e68ca87f58ecd5f0b4a3c24ca38e56eaf61e34
cubocode <cubo.code.dev@gmail.com>
2026-05-03 15:33:53 -0300

    Agregar archivos de proyecto.

---
+++ proyectoHabitar/Ventanas/Login.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace proyectoHabitar.Ventanas
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnIngresar_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUsuario.Text == "admin" && txtPassword.Text == "1234")
+            { 
+                proyectoHabitar.Ventanas.Home ventanaHome = new proyectoHabitar.Ventanas.Home();
+                ventanaHome.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Credenciales incorrectas", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
